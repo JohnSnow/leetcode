9b276bf8356c7c45e42ab4c35d08cea4d1e2aabf
Jin Huan <huan.jin@hp.com>
2014-12-11 22:30:05 +0800

    implement strStr

---
+++ strStr/strStr.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+class Solution:
+    # @param haystack, a string
+    # @param needle, a string
+    # @return an integer
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)

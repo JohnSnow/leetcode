a6c9ac2fbaafbd80898d098ff93a9819b1a0b888
Huan Jin <huan.jin@hp.com>
2014-12-18 22:52:01 +0800

    update LRUCache, with stl. 392 ms, need improvement

--- LRUCache/LRUCache.cpp
+++ LRUCache/LRUCache.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <list>
+#include <utility>
 
 using std::map;
-using std::min_element;
 
 class LRUCache{
 public:
@@ -11,66 +12,92 @@ public:
     }
 
     int get(int key) {
+        std::list<std::pair<int, int> >::iterator _lru_it;
+        map<int, std::list< std::pair<int, int> >::iterator >::iterator _cache_it;
         _cache_it = _cache.find(key);
         if (_cache_it == _cache.end()) {
             return -1;
         } else {
-            return _cache_it->second;
-            _freq[_cache_it->first] += 1;
+            _lru_it = _cache_it->second;
+            update_exist_lru(_lru_it);
+            _cache[key] = _lru.begin();
+            return _lru.front().second;
         }
     }
 
     void set(int key, int value) {
 
-        map<int, int>::iterator _freq_it;
-
+        bool full = false;
+        std::list<std::pair<int, int> >::iterator _lru_it;
+        map<int, std::list<std::pair<int, int> >::iterator >::iterator _cache_it;
         _cache_it = _cache.find(key);
+
         if (_cache_it != _cache.end()) {
-            _cache_it->second = value;
-            _freq[_cache_it->first] += 1;
+            _lru_it = _cache_it->second;
+            update_exist_lru(_lru_it, value);
+            _cache[key] = _lru.begin();
+
         } else {
             if (cnt == cap) {
-                _freq_it = min_element(_freq.begin(), _freq.end(), LessBySecond);
-                _cache.erase(_freq_it->first);
+                _cache.erase(_lru.back().first);
                 cnt--;
+                full = true;
             }
-            _cache[key] = value;
-            _freq[key] = 1;
             cnt++;
+            update_new_lru(std::pair<int, int> (key, value), full);
+            _cache[key] = _lru.begin();
         }
+    }
+
+    void update_exist_lru(std::list<std::pair<int, int> >::iterator it)
+    {
+        int key = (*it).first;
+        int value = (*it).second;
+        _lru.erase(it);
+        _lru.push_front(std::pair<int, int> (key, value));
+    }
+
+    void update_exist_lru(std::list<std::pair<int, int> >::iterator it, int value)
+    {
+        int key = (*it).first;
+        _lru.erase(it);
+        _lru.push_front(std::pair<int, int> (key, value));
+    }
+
+    void update_new_lru(std::pair<int, int> kp, bool full)
+    {
+        if(full) {
+            _lru.pop_back();
+        }
+        _lru.push_front(kp);
     }
 
     void printElem() {
-        for (map<int, int>::iterator it =  _cache.begin(); it != _cache.end(); it++) {
-            std::cout << it->second  << " " << std::endl;
+        std::cout << "==========================" << std::endl;
+        for (std::list<std::pair<int, int> >::iterator it =  _lru.begin(); it != _lru.end(); it++) {
+            std::cout << (*it).first << "-> " << (*it).second  << " " << std::endl;
         }
+        std::cout << "*************************" << std::endl;
     }
-
-    template <typename Lhs, typename Rhs>
-    bool LessBySecond(const Lhs &lhs, const Rhs &rhs)
-    {
-        return lhs.second < rhs.second;
-    };
 
 private:
     size_t cap;
     size_t cnt;
 
-    map<int, int> _cache;
-    map<int, int> _freq;
-    map<int, int>::iterator _cache_it;
+    map<int, std::list<std::pair<int, int> >::iterator > _cache;
 
+    std::list<std::pair<int, int> > _lru;
 };
+
 
 int main(int argc, const char *argv[])
 {
-    LRUCache *cache = new LRUCache(2);
-    cache->set(1, 10);
-    cache->set(2,5);
-    cache->get(1);
-    cache->printElem();
-    cache->set(3,3);
-    cache->printElem();
+    LRUCache *cache = new LRUCache(1);
+    cache->set(2, 1);
+    std::cout <<  cache->get(2) << std::endl;
+    cache->set(3,2);
+    std::cout <<  cache->get(2) << std::endl;
+    std::cout <<  cache->get(3) << std::endl;
 
 
     return 0;
